93d9ae2b2354ad2ad44de3d4a7e171de5d853ede
Michael Fields <michael@mfields.org>
2011-08-21 18:48:51 -0700

    No need for variable

--- taxonomy-list-shortcode-index.php
+++ taxonomy-list-shortcode-index.php
@@ -16,7 +16,6 @@ $o.= "\n\t" . '<div class="mf_taxonomy_list">';
 foreach ( $chunked as $k => $column ) {
 	$o.= "\n\t" . '<ul class="mf_taxonomy_column mf_cols_' . $args['cols'] . '">';
 	foreach ( $column as $term ) {
-		$url = get_term_link( $term, $term->taxonomy );
 		$count = intval( $term->count );
 		$style = '';
 		$style.= ( $args['background'] != 'fff' ) ? ' background:#' . $args['background'] . ';' : '';
@@ -26,7 +25,7 @@ foreach ( $chunked as $k => $column ) {
 		$class = ( $args['show_counts'] ) ? ' class="has-quantity"' : '';
 		$quantity = ( $args['show_counts'] ) ? ' <span' . $style . ' class="quantity">' . $count . '</span>' : '';
 
-		$o.= "\n\t\t" . '<li' . $class . $style . '><a' . $style . ' class="term-name" href="' . esc_url( $url ) . '">' . esc_html( $term->name ) . '</a>' . taxonomy_list_shortcode_edit_term_link( $term ) . '' . $quantity . '</li>';
+		$o.= "\n\t\t" . '<li' . $class . $style . '"><a' . $style . ' class="term-name" href="' . get_term_link( $term, $term->taxonomy ) . '">' . esc_html( $term->name ) . '</a>' . taxonomy_list_shortcode_edit_term_link( $term ) . '' . $quantity . '</li>';
 	}
 	$o.=  "\n\t" . '</ul>';
 }
